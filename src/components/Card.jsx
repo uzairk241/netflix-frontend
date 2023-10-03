bb7241b421807dc6b8fef92feb8e7e07bbde1f00
uzairk241 <m.uzairkhan241@gmail.com>
2023-10-03 20:33:21 +0500

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -26,7 +26,7 @@ export default React.memo(function Card({ index, movieData, isLiked = false }) {
 
   const addToList = async () => {
     try {
-      await axios.post("http://localhost:5000/api/user/add", {
+      await axios.post("https://netflix-backend-7b7v9vgvn-uzairk241.vercel.app/api/user/add", {
         email,
         data: movieData,
       });
